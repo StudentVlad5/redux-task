131055af3c1f0a344c6480af52bce4c1dde8f4bc
Vlad <vlad_np@ukr.net>
2024-08-20 17:18:31 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { App } from "components/App";
+import { store } from "./redux/store";
 import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-
-
